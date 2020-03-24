1e31b6521146b83de2e5084b41e64f305878b310
Alec Dye <alecdye@gmail.com>
2020-03-24 14:53:53 -0700

    Building Data rendering components

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,10 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+//Components
+import FriendCard from "./FriendCard";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const FriendList = () => {
   const [friends, setFriends] = useState([]);
+
+  const getData = () => {
+    const token = window.localStorage.getItem("token");
+    axiosWithAuth()
+      .get("friends")
+      .then(res => {
+        console.log(res);
+        setFriends(res.data);
+      })
+      .catch(err => {
+        console.err(err);
+      });
+    useEffect(() => {
+      getData();
+    }, []);
+  };
+
   return (
     <div>
       <h1>Friends!</h1>
+      <div>
+        {friends.map(friend => {
+          <FriendCard key={key} friend={friend} />;
+        })}
+      </div>
     </div>
   );
 };
