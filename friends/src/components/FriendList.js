a4746a7b1cf32f25d3e79b037dc18e15095edbbd
Alec Dye <alecdye@gmail.com>
2020-03-24 14:43:42 -0700

    Building out components

---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const FriendList = () => {
+  const [friends, setFriends] = useState([]);
+  return (
+    <div>
+      <h1>Friends!</h1>
+    </div>
+  );
+};
+
+export default FriendList;
